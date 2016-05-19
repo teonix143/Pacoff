2ec464119033ee9d62591911d1f82cbade7bce01
teonix143 <teonix143@gmail.com>
2016-05-19 09:58:15 +0800

    Add files via upload

---
+++ back.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class back : MonoBehaviour {
+
+	public Renderer rend;
+	public bool backtoMainMenu;
+	public bool backPlayMenu;
+	public bool backLevelMenu;
+
+
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		rend.enabled = true;
+	}
+	void OnMouseEnter(){
+		rend.material.color = Color.yellow;
+	}
+
+	void OnMouseExit(){
+		rend.material.color = Color.white;
+	}
+	
+	void OnMouseDown(){
+		if (backtoMainMenu) 
+		{
+			Application.LoadLevel (0);
+		} 
+		else if (backPlayMenu) 
+		{
+			Application.LoadLevel (1);
+		}
+		else if (backLevelMenu) 
+		{
+			Application.LoadLevel (3);
+		}
+	}
+}
